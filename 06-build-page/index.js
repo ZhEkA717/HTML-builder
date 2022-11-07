753c1065405940077c8fc02d883bc98693b7f7b8
Zheka <grushevskiy.yevgeniy@mail.ru>
2022-11-08 02:43:29 +0300

    feat: 06-build-page is decided

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const fs = require("fs");
+const {
+    deepStrictEqual
+} = require("assert");
 
 let strHtmlContent = new Promise(resolve => {
     const arrPatternReplace = [];
@@ -155,4 +158,85 @@ Promise.all([strHtmlContent, bundleCssFiles, createFolderAndFiles])
                 })
             }
         )
-    })
+
+        return new Promise(resolve => {
+            fs.mkdir(path.join(__dirname, 'project-dist', "assets"), {
+                    recursive: true
+                },
+                err => {
+                    if (err) throw err;
+                    console.log("folder 'assets' was created");
+                    resolve();
+                });
+        })
+    }).then(() => {
+        return new Promise(resolve => {
+            function deleteFolderAndFiles(filePwd) {
+                fs.readdir(
+                    path.join(__dirname, filePwd),
+                    (err, files) => {
+                        if (err) throw err;
+                        if (files.length) {
+                            files.forEach(item => {
+                                fs.stat(path.join(__dirname, filePwd, item),
+                                    (err, stats) => {
+                                        if (err) throw err;
+                                        if (stats.isFile()) {
+                                            fs.unlink(path.join(__dirname, filePwd, item),
+                                                err => {
+                                                    if (err) throw err;
+                                                    console.log(`Файл ${item} успешно удален`);
+                                                });
+                                        } else {
+                                            deleteFolderAndFiles(`${filePwd}/${item}`);
+                                        }
+                                    }
+                                );
+                            });
+                        } else {
+                            fs.rmdir(path.join(__dirname, filePwd), err => {
+                                if (err) throw err;
+                                console.log('Папка успешно удалена');
+                            });
+                        }
+                        resolve();
+                    });
+            }
+            deleteFolderAndFiles('project-dist/assets');
+        });
+    }).then(() => {
+        function readAndCopyFile(filePwd) {
+            fs.readdir(
+                path.join(__dirname, filePwd),
+                (err, elements) => {
+                    if (err) throw err;
+                    elements.forEach(item => {
+                        fs.stat(path.join(__dirname, filePwd, item),
+                            (error, stats) => {
+                                if (error) throw error;
+                                if (stats.isFile()) {
+                                    fs.copyFile(
+                                        path.join(__dirname, filePwd, item),
+                                        path.join(__dirname, "project-dist", filePwd, item),
+                                        err => {
+                                            if (err) throw err;
+                                            console.log(`file "${item}"  was copied`);
+                                        }
+                                    );
+                                } else {
+                                    fs.mkdir(path.join(__dirname, 'project-dist', `${filePwd}/${item}`), {
+                                            recursive: true
+                                        },
+                                        err => {
+                                            if (err) throw err;
+                                            console.log("folder 'assets' was created");
+                                            readAndCopyFile(`${filePwd}/${item}`);
+                                        });
+                                }
+                            })
+                    })
+
+                });
+        }
+        readAndCopyFile("assets");
+    });
